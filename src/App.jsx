df199c49dad0cbc7b601a0226b88bb08fc4134c0
Tanushri Jain <jaintanushri34@gmail.com>
2023-11-06 21:27:11 -0600

    code changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,30 @@ import './App.css';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log('puppyList: ', puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
   return (
     <div className='app'>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
       })}
     </div>
   );
